f0f3f8c9c0c1a01baa702898bedc90695821da5e
Emilio Wuerges <wuerges@gmail.com>
2017-02-23 18:52:29 -0300

    ok

--- NetCloser/src/DrawShapes.hs
+++ NetCloser/src/DrawShapes.hs
@@ -17,14 +17,14 @@ drawRect (R (P (x1, y1)) (P (x2, y2))) =
    in fill $ rectangle (V2 x1' y1') (x2' - x1') (y2' - y1')
 
 drawRoutedShape (Shape r) =
-  let color = PixelRGBA8 0xFF 0x53 0x73 200
+  let color = PixelRGBA8 0x00 0xBF 0xFF 200
    in withTexture (uniformTexture color) $ drawRect r
 
 
 drawVia (Via (P (x, y))) =
-  let color = PixelRGBA8 0 0x86 0xc1 255
+  let color = PixelRGBA8 0 0 0xff 255
    in withTexture (uniformTexture color) $
-     fill $ circle (V2 (fi x) (fi y)) 1
+     fill $ circle (V2 (fi x) (fi y)) 5
 
 drawObstacle (Obstacle r) =
   let color = PixelRGBA8 0xDC 0xDC 0xDC 200
