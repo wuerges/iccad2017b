a5e2b8745ed4842065c87fbaf6111d44035765e0
Emilio Wuerges <wuerges@gmail.com>
2017-02-26 19:56:20 -0300

    Generated input segments

--- NetCloser/src/Hanan.hs
+++ NetCloser/src/Hanan.hs
@@ -3,6 +3,19 @@ module Hanan where
 
 import Geometry
 import Shapes
+import Data.List as L
+
+segments3d _ (LayerN l
+  , Shape (R (P (x, y)) (P (x', y')))) =
+    [ (P3 x y l, P3 x y' l)
+    , (P3 x y l, P3 x' y l)
+    , (P3 x y' l, P3 x' y' l)
+    , (P3 x' y l, P3 x' y' l) ]
+
+segments3d _ (_, Obstacle _) = []
+
+segments3d p (LayerN l, Via (P (x, y))) =
+  [ (P3 x y (l * spacing p), P3 x y ((l+1) * spacing p)) ]
 
 points3dShape _ (LayerN l
   , Shape (R (P (x, y)) (P (x', y')))) =
@@ -11,22 +24,23 @@ points3dShape _ (LayerN l
   , P3 x' y l
   , P3 x' y' l ]
 
-points3dShape _ (LayerN l, Via (P (x, y))) =
-  [ P3 x y l
-  , P3 x y (l+1) ]
+points3dShape p (LayerN l, Via (P (x, y))) =
+  [ P3 x y (l * spacing p)
+  , P3 x y ((l+1) * spacing p) ]
 
 
-points3dShape s (LayerN l
+points3dShape p (LayerN l
                 , Obstacle
                           (R (P (x, y)) (P (x', y')))) =
                             [ P3 ox  oy  l
                             , P3 ox  oy' l
                             , P3 ox' oy  l
                             , P3 ox' oy' l ]
- where ox = x - s
-       oy = y - s
-       ox' = x' + s
-       oy' = y' + s
+                              where s = spacing p
+                                    ox = x - s
+                                    oy = y - s
+                                    ox' = x' + s
+                                    oy' = y' + s
 
 points3dShape _ _            = error "This function should not be used here"
 
@@ -35,15 +49,16 @@ hananPs ps =
   [ P3 px py pz
     | px <- map x ps, py <- map y ps, pz <- map z ps ]
 
-
-
 points3d :: Problem -> [Point3D]
 points3d p = map (setViaCost (viaCost p)) ps
   where
-    ps = concatMap (points3dShape (spacing p)) (pelements p ++ pvias p)
+    ps = concatMap (points3dShape p) (pelements p ++ pvias p)
     setViaCost vc (P3 x y z) = (P3 x y (z * vc))
 
-hanan = hananPs . points3d
+rmdups = L.map L.head . L.group . L.sort
+
+hanan =
+  rmdups . hananPs . points3d
 
 
 
